62fd2ef7f665f82a42c8eac446db454334288a28
Kristina Krylova <kriskamumu@gmail.com>
2021-12-25 00:29:15 +0300

    additional task: make tests for ACCEPT ORDER

--- src/main/java/ru/yandex/samokat/client/OrderClient.java
+++ src/main/java/ru/yandex/samokat/client/OrderClient.java
@@ -29,4 +29,27 @@ public class OrderClient extends AbstractRestAssuredClient {
                 .get(ORDER_PATH)
                 .then().log().all();
     }
+
+    @Step("Send PUT request to /api/v1/orders/accept/:{orderId} to accept the order " +
+            "by a courier with ID = {courierId}")
+    public ValidatableResponse acceptOrder(String orderId, String courierId) {
+        return given()
+                .spec(getBaseSpec())
+                .queryParam("courierId", courierId)
+                .log().all()
+                .when()
+                .put(ORDER_PATH + "/accept/" + orderId)
+                .then().log().all();
+    }
+
+    @Step("Send GET request to /api/v1/orders to get order id by track = {track}")
+    public ValidatableResponse getOrderByTrack(String track) {
+        return given()
+                .spec(getBaseSpec())
+                .queryParam("t", track)
+                .log().all()
+                .when()
+                .get(ORDER_PATH + "/track")
+                .then().log().all();
+    }
 }
